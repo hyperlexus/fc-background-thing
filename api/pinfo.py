7db482d562ea5d5290848b15b25dfb968cef4f43
HyperLexus <you@example.com>
2026-02-28 22:13:52 +0100

    4

--- api/pinfo.py
+++ api/pinfo.py
@@ -8,11 +8,18 @@ CORS(app)
 
 @app.route('/api/pinfo', methods=['POST'])
 def get_pinfo():
-    data = request.get_json()
-    pid = data.get('pid')
+    try:
+        requests.get("https://google.com", timeout=2)
+    except Exception as e:
+        return jsonify({"error": "Vercel has no internet access", "details": str(e)}), 500
 
     try:
-        response = requests.post("http://rwfc.net/api/pinfo", json={"pid": pid})
+        data = request.get_json()
+        pid = data.get('pid')
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+        response = requests.post("https://rwfc.net/api/pinfo", json={"pid": pid}, headers=headers)
         return jsonify(response.json())
     except Exception as e:
         return jsonify({"error": str(e)}), 500
